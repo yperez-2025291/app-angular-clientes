7912345fc805053b01f6a95aa5724a58e1de999a
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-07-14 11:24:06 -0600

    feat: agrega backend y frontend del proyecto

---
+++ frontend/main.ts
@@ -0,0 +1,5 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './src/app.components';
+
+bootstrapApplication(AppComponent)
+  .catch((err) => console.error(err));
---
+++ frontend/src/app.components.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>¡Aplicación de Tareas Funcionando!</h1>
+    <p>El frontend de Angular se ha levantado correctamente.</p>
+  `,
+  styles: []
+})
+export class AppComponent {}
